ef6878229cf10fd8c10306eea4bc842f2af6d93a
Víctor de Souza Faria <victor_sfaria@yahoo.com.br>
2015-08-21 15:57:22 -0300

    Agora em português :)
    Primeira versão do commit em português.

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -4,7 +4,7 @@ public class HelloWorld
 {
    public static int Main(string[] args)
    {
-      Console.WriteLine("Hello, GitHub!");
+      Console.WriteLine("Olá Mundo, só caisando $$$%%%é");
       return 0;
    }
 }
